372e7d77f53704a6f48784616225a6aefbf4084a
Huong <admin@Apples-MacBook-Pro.local>
2018-06-18 21:17:14 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,44 @@
 |
 */
 
-Route::get('/', function () {
+Route::get('index', function () {
     return view('welcome');
+});
+Route::get('detail-{id}-{alias}',function($ms, $alias){
+    ///return 1;
+    echo $ms;
+    echo $alias;
+})->where([
+    'id' => '[0-9]+',
+    'alias' => '[a-z]+'
+]);
+
+Route::get('news/{id}',function($id){
+    echo $id;
+})->name('news');
+
+Route::get('list-product/{page?}',function($page=1){
+    //echo 23232;
+    echo $page;
+})->where('page','[0-9]+')
+->name('list-product');
+
+Route::get('call-route',function(){
+    //return redirect()->route('list-product');
+
+    return redirect()->route('news',['id'=>121]);
+
+    //return redirect('/news/121');
+});
+
+Route::group(['prefix'=>'admin'],function(){
+
+    // admin/index
+    Route::get('index',function (){
+        return 1;
+    });
+
+    Route::get('detail',function (){
+        return 1;
+    });
 });
